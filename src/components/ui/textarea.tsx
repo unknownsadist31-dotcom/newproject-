a64aaef77a6ad99a5860a9f45d45277fce21bfdd
unknownsadist31-dotcom <unknownsadist31@gmail.com>
2026-07-28 23:45:35 -0400

    Update textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,7 +6,7 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
     <textarea
       data-slot="textarea"
       className={cn(
-        'placeholder:text-andy aria-invalid:border-lucian flex field-sizing-content w-full resize-none rounded-xl border p-4 text-base font-medium break-words outline-none disabled:cursor-not-allowed disabled:opacity-50',
+        'placeholder:text-andy aria-invalid:border-lucian flex field-sizing-content max-h-28 min-h-14 w-full resize-none overflow-y-auto rounded-xl border p-4 text-base font-medium break-all outline-none disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       {...props}
